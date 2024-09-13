72ac1fae62a38ada5a89265ad1c288f86c5e86d9
sabrinasanmi <sanmisabrina@icloud.com>
2024-09-13 00:19:53 -0300

    Ajuste config. banco de dados

--- config.py
+++ config.py
@@ -2,8 +2,5 @@ import os
 
 class Config:
     # Configuração do banco de dados
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        'DATABASE_URL', 
-        'postgresql://escola_s01u_user:1GeK4fpqXqJc2Ohuf8hdmmz3kAffqcbA@dpg-crhp22lumphs73c6t3qg-a.oregon-postgres.render.com/escola_s01u'
-    )
+    SQLALCHEMY_DATABASE_URI = 'postgresql://escola_s01u_user:1GeK4fpqXqJc2Ohuf8hdmmz3kAffqcbA@dpg-crhp22lumphs73c6t3qg-a.oregon-postgres.render.com/escola_s01u'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
